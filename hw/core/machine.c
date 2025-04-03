393c074a5f2f5946d3558041856bab3d55ac5cdd
Philippe Mathieu-Daudé <philmd@linaro.org>
2025-04-04 00:54:15 +0200

    hw/core/machine: Allow dynamic registration of valid CPU types
    Add MachineClass::get_valid_cpu_types(), a helper that
    returns a dynamic list of CPU types. Since the helper
    takes a MachineState argument, we know the machine is
    created by the time we call it.
    
    Suggested-by: Pierrick Bouvier <pierrick.bouvier@linaro.org>
    Signed-off-by: Philippe Mathieu-Daudé <philmd@linaro.org>

--- hw/core/machine.c
+++ hw/core/machine.c
@@ -1583,6 +1583,32 @@ static bool is_cpu_type_supported(const MachineState *machine, Error **errp)
 
             return false;
         }
+    } else if (mc->get_valid_cpu_types) {
+        GPtrArray *vct = mc->get_valid_cpu_types(machine);
+        bool valid = false;
+
+        for (i = 0; i < vct->len; i++) {
+            if (object_class_dynamic_cast(oc, vct->pdata[i])) {
+                valid = true;
+                break;
+            }
+        }
+
+        if (!valid) {
+            g_autofree char *requested = cpu_model_from_type(machine->cpu_type);
+
+            error_setg(errp, "Invalid CPU model: %s", requested);
+            error_append_hint(errp, "The valid models are: ");
+            for (i = 0; i < vct->len; i++) {
+                g_autofree char *model = cpu_model_from_type(vct->pdata[i]);
+                error_append_hint(errp, "%s%s",
+                                  model, i + 1 == vct->len ? "\n" : ", ");
+            }
+        }
+        g_ptr_array_free(vct, true);
+        if (!valid) {
+            return false;
+        }
     }
 
     /* Check if CPU type is deprecated and warn if so */
