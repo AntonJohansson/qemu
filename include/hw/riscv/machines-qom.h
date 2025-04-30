029b3f344e533b7586dbd6e19142c44f64cad7fa
Anton Johansson <anjo@rev.ng>
2025-04-30 13:44:17 +0200

    hw/riscv: Add DEFINE_MACHINE_RISCV[32|64|32_64]() macros
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- include/hw/riscv/machines-qom.h
+++ include/hw/riscv/machines-qom.h
@@ -17,4 +17,20 @@
 #define TYPE_TARGET_RISCV64_MACHINE \
         "target-info-riscv64-machine"
 
+extern InterfaceInfo riscv32_machine_interfaces[];
+extern InterfaceInfo riscv64_machine_interfaces[];
+extern InterfaceInfo riscv32_64_machine_interfaces[];
+
+#define DEFINE_MACHINE_RISCV32(namestr, machine_initfn) \
+        DEFINE_MACHINE_WITH_INTERFACES(namestr, machine_initfn, \
+                                       riscv32_machine_interfaces)
+
+#define DEFINE_MACHINE_RISCV64(namestr, machine_initfn) \
+        DEFINE_MACHINE_WITH_INTERFACES(namestr, machine_initfn, \
+                                       riscv64_machine_interfaces)
+
+#define DEFINE_MACHINE_RISCV32_64(namestr, machine_initfn) \
+        DEFINE_MACHINE_WITH_INTERFACES(namestr, machine_initfn, \
+                                       riscv32_64_machine_interfaces)
+
 #endif
