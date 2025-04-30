dd4bf1fa807c5b00a59f32b52ab9e1e986ca9ffc
Anton Johansson <anjo@rev.ng>
2025-04-30 13:34:40 +0200

    hw/riscv: Register TYPE_TARGET_RISCV32/64_MACHINE QOM interfaces
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- target-info-qom.c
+++ target-info-qom.c
@@ -9,6 +9,7 @@
 #include "qemu/osdep.h"
 #include "qom/object.h"
 #include "hw/arm/machines-qom.h"
+#include "hw/riscv/machines-qom.h"
 
 static const TypeInfo target_info_types[] = {
     {
@@ -17,6 +18,14 @@ static const TypeInfo target_info_types[] = {
     },
     {
         .name           = TYPE_TARGET_AARCH64_MACHINE,
+        .parent         = TYPE_INTERFACE,
+    },
+    {
+        .name           = TYPE_TARGET_RISCV32_MACHINE,
+        .parent         = TYPE_INTERFACE,
+    },
+    {
+        .name           = TYPE_TARGET_RISCV64_MACHINE,
         .parent         = TYPE_INTERFACE,
     },
 };
