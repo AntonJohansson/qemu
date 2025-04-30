78d248ec8a68a2354389ddc7b5473a70c7f6fac3
Anton Johansson <anjo@rev.ng>
2025-04-30 14:29:40 +0200

    configs/targets: Define target_arch
    Signed-off-by: Anton Johansson <anjo@rev.ng>

--- configs/targets/riscv32-softmmu.c
+++ configs/targets/riscv32-softmmu.c
@@ -10,9 +10,11 @@
 #include "qemu/target-info-impl.h"
 #include "hw/riscv/machines-qom.h"
 #include "target/riscv/cpu-qom.h"
+#include "qapi/qapi-types-machine.h"
 
 static const TargetInfo target_info_riscv32_system = {
     .target_name = "riscv32",
+    .target_arch = SYS_EMU_TARGET_RISCV32,
     .machine_typename = TYPE_TARGET_RISCV32_MACHINE,
 };
 
